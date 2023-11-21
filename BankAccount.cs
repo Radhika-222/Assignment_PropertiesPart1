3b471ae5c6963c5956798f62f06d58c85b5e8176
Radhika-222 <wandareradhika@gmail.com>
2023-11-21 16:08:54 +0530

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_PropertiesPart1
+{
+    public class BankAccount
+    {
+        public int AccountNo { get; }
+
+        public string AccountHolderName { get; set; }
+
+        private double balance;
+
+        public double Balance
+        {
+            get { return balance; }
+            private set { balance = value; }
+        }
+
+        public BankAccount(int accountNo, string accountHolderName)
+        {
+            this.AccountNo = accountNo;
+            this.AccountHolderName = accountHolderName;
+            Balance = 0;
+        }
+
+        public void Deposit(double amount)
+        {
+            if(amount>0)
+            {
+                Balance += amount;
+                Console.WriteLine($"Deposited{amount}. New balance:{Balance}");
+
+            }
+            else
+            {
+                Console.WriteLine("Deposite amount must be greater than zero.");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if(amount>0 && amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdraw{amount} New Balance{Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid withdrawal amount or insufficient balance");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_PropertiesPart1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(123456789, "Sam");
+            account.Deposit(50000);
+            account.Withdraw(10000);
+
+            Console.WriteLine("Account Number:" +account.AccountNo);
+            Console.WriteLine("Account Holder Name:"+account.AccountHolderName);
+            Console.WriteLine("Current Balance:" +account.Balance);
+            Console.ReadKey();
+        }
+    }
+}
